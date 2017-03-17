a76fca2e2e031ff725823df50c714bc98a30ef89
ASTPMeetup <pretto417@gmail.com>
2017-03-16 22:01:04 -0500

    added accessibility tags

--- public/javascript.js
+++ public/javascript.js
@@ -48,16 +48,16 @@ $('.button').hover(function(){ $(this).css('opacity', '0.7');}, function(){ $(th
         e.preventDefault();
         var id = $(this).attr('href');
         var car = $(this).closest('ul');
-        var year_details = $(this).siblings("#item1").text();
-        var color_details = $(this).siblings("#item2").text();
-        var make_details = $(this).siblings("#item3").text();
-        var mileage_details = $(this).siblings("#item4").text().slice(9, 17);
-        var edit_car_form = '<img src="del.jpg" id="'+id+'" class="delete_img"><form name="car_edit" class="car_edit">'+
-            '<input name="year" type="text" id="year_edit" maxlength="4" value="'+year_details+'" required>'+
-            '<input name="color" type="text" id="color_edit" value="'+color_details+'" required>'+
-            '<input name="make" type="text" id="make_edit" value="'+make_details+'" required>'+
-            '<input name="mileage" type="text" id="mileage_edit" value="'+mileage_details+'" required>'+
-            '<input type="submit" value="update" class="button" id="update_submit"></form>';
+        var year_details = $(this).siblings(".item1").text();
+        var color_details = $(this).siblings(".item2").text();
+        var make_details = $(this).siblings(".item3").text();
+        var mileage_details = $(this).siblings(".item4").text().slice(9, 17);
+        var edit_car_form = '<img role="button" alt="delete car button" src="del.jpg" id="'+id+'" class="delete_img"><form name="car_edit" class="car_edit">'+
+            '<input aria-label="year edit" name="year" type="text" id="year_edit" maxlength="4" value="'+year_details+'" required>'+
+            '<input aria-label="color edit" aname="color" type="text" id="color_edit" value="'+color_details+'" required>'+
+            '<input aria-label="make and model edit" name="make" type="text" id="make_edit" value="'+make_details+'" required>'+
+            '<input aria-label="mileage edit" name="mileage" type="text" id="mileage_edit" value="'+mileage_details+'" required>'+
+            '<input role="button" type="submit" value="update" class="button" id="update_submit"></form>';
 
         car.replaceWith(edit_car_form);
         car.replaceWith(car);
@@ -86,9 +86,9 @@ $('.button').hover(function(){ $(this).css('opacity', '0.7');}, function(){ $(th
 
   function printCarListing(car){
     var newCar = '<div class="row" id="' + car.make + car._id.substring(10,13) + '">'+
-      '<ol class="col-md-12" id="car_div"><ul style="padding-left: 5px;"><img src="del.jpg" id="'+car._id+'" class="delete_img">'+
-      '<span id="item1">'+car.year+'</span> <span id="item2">'+car.color+'</span> <span id="item3">'+car.make+'</span>'+
-      ' (<span id="item4">mileage: '+car.mileage+'</span>) <a href="'+car._id+'" class="editLink">update</a></ol></li>'+
+      '<ol role="list" class="col-md-12" class="car_div"><ul style="padding-left: 5px;"><img role="button" alt="delete car button" src="del.jpg" id="'+car._id+'" class="delete_img">'+
+      '<span role="listitem" class="item1">'+car.year+'</span> <span role="listitem" class="item2">'+car.color+' </span><span role="listitem" class="item3">'+car.make+'</span>'+
+      ' (<span role="listitem" class="item4">mileage: '+car.mileage+'</span>) <a role="link" alt="update car link" href="'+car._id+'" class="editLink">update</a></ol></li>'+
       '<hr></div>';
     return newCar;
   }
